5c51ebf69f7bd9eea7cf23b4933b7a8f82da9b98
ElenaGrib <elenagrib27@gmail.com>
2022-08-13 18:10:36 +0300

    Initial commit

---
+++ src/test/java/com/grib/discovery/server/photoappeurekadiscoveryserver/PhotoAppEurekaDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grib.discovery.server.photoappeurekadiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppEurekaDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
